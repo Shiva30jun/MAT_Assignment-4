6d0714f70a7030eb37e9b1cecb083a809721dfbc
root <root@Shiva.>
2025-04-09 15:26:58 -0400

    Added test case for paintRequiredCalculator with area 50 and coveragePerLiter 0 returning Infinity

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -9,4 +9,9 @@ describe("paintRequiredCalculator", () => {
   test("Verify that paintRequiredCalculator should return 0 when area is 0 and coveragePerLiter is 10", () => {
     expect(paintRequiredCalculator(0, 10)).toBe(0);
   });
+
+  test("Verify that paintRequiredCalculator should return Infinity when area is 50 and coveragePerLiter is 0", () => {
+    expect(paintRequiredCalculator(50, 0)).toBe(Infinity);
+  });
+
 });
