56ea6eb4d172bee9810b4beaf6dc93492f860027
root <root@Shiva.>
2025-04-09 15:15:10 -0400

    Added test case for estimatePaintingTime with area 100 and paintingSpeed 15 returning approx. 6.67

--- src/estimatePaintingTime/estimatePaintingTime.test.ts
+++ src/estimatePaintingTime/estimatePaintingTime.test.ts
@@ -32,4 +32,9 @@ describe('estimatePaintingTime', () => {
   test('Verify that estimatePaintingTime should return 10 when area is -100 and paintingSpeed is -10', () => {
     expect(estimatePaintingTime(-100, -10)).toBe(10);
   });
+
+  test('Verify that estimatePaintingTime should return approx. 6.67 when area is 100 and paintingSpeed is 15', () => {
+    expect(estimatePaintingTime(100, 15)).toBeCloseTo(6.6667, 4);
+  });
+  
 });
